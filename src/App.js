0df10c2627491a182d5353e96cd7ef137b24934e
datbh <bdat1606@gmail.com>
2023-09-18 11:35:08 +0700

    fix: resource not found
    Change the baseURL to localhost

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
 import api from './api/axiosConfig';
-import {useState, useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import Layout from './components/Layout';
-import {Routes, Route} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
@@ -16,56 +16,43 @@ function App() {
     const [reviews, setReviews] = useState([]);
 
     const getMovies = async () => {
-
         try {
-
-            const response = await api.get("/api/v1/movies");
-
+            const response = await api.get("/movies");
             setMovies(response.data);
-
         } catch (err) {
             console.log(err);
         }
     }
 
     const getMovieData = async (movieId) => {
-
         try {
-            const response = await api.get(`/api/v1/movies/${movieId}`);
-
+            const response = await api.get(`/movies/${movieId}`);
             const singleMovie = response.data;
-
             setMovie(singleMovie);
-
             setReviews(singleMovie.reviews);
-
-
         } catch (error) {
             console.error(error);
         }
-
     }
 
     useEffect(() => {
-        getMovies();
+        getMovies().then(r => console.log(r));
     }, [])
 
-    return (
-        <div className="App">
-            <Header/>
-            <Routes>
-                <Route path="/" element={<Layout/>}>
-                    <Route path="/" element={<Home movies={movies}/>}></Route>
-                    <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-                    <Route path="/Reviews/:movieId"
-                           element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews}
-                                             setReviews={setReviews}/>}></Route>
-                    <Route path="*" element={<NotFound/>}></Route>
-                </Route>
-            </Routes>
+    return (<div className="App">
+        <Header/>
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route path="/" element={<Home movies={movies}/>}></Route>
+                <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+                <Route path="/Reviews/:movieId"
+                       element={<Reviews getMovieData={getMovieData} movie={movie} reviews={reviews}
+                                         setReviews={setReviews}/>}></Route>
+                <Route path="*" element={<NotFound/>}></Route>
+            </Route>
+        </Routes>
 
-        </div>
-    );
+    </div>);
 }
 
 export default App;
